9dbc5d7c9f84b3db625eaa6fb0f93ed977ff19a6
Leonard Ferrer <leonnardo.fer@gmail.com>
2022-11-20 16:26:34 -0300

    criando metodo para logar na rede wifi

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,10 @@
 #include <ESPAsyncTCP.h>
 #include <ESPAsyncWebServer.h>
 #include "LittleFS.h"
+# include "ArduinoJson.h"
+
+AsyncWebServer server(80);
+
 
 //###################################################################
 //Declaração de funções
@@ -8,15 +12,17 @@
 void initFS();
 String fileRead(const char* path);
 void fileWrite(const char* path, const char* content);
-void json();
-
+bool modeStation();
+void modeAccessPoint();
 
 //###################################################################
 
 
 void setup(){
     Serial.begin(9600);
-    initFS();    
+    initFS();   
+
+    modeStation(); 
 }
 
 
@@ -30,8 +36,8 @@ void loop(){
 
 /// @brief Inicia o sistema de arquivos LittlesFS
 void initFS(){
-    if(LittleFS.begin())    Serial.println(F("LittesFS iniciado com sucesso!"));
-    else                    Serial.println(F("Falha ao iniciar o LittleFS!"));
+    if(LittleFS.begin())    Serial.println(F("\nLittesFS iniciado com sucesso!"));
+    else                    Serial.println(F("\nFalha ao iniciar o LittleFS!"));
 }
 
 
@@ -79,3 +85,49 @@ void fileWrite(const char* path, const char* content){
 
 //###################################################################
 
+bool modeStation(){
+    StaticJsonBuffer<200> jsonBuffer;
+    String json = fileRead("/network_settings.json");
+    JsonObject& root = jsonBuffer.parseObject(json);
+    if(!root.success()){
+        Serial.println(F("Ero ao carregar as configurações de rede."));
+        return false;
+    }
+
+    if( !(root["ip"].asString() == "") && !(root["gateway"].asString() == "") && !(root["subnet"].asString() == "")){
+        IPAddress ip;
+        IPAddress gateway;
+        IPAddress subnet;
+        ip.fromString(root["ip"].asString());
+        gateway.fromString(root["gateway"].asString());
+        subnet.fromString(root["subnet"].asString());    
+
+        if(!WiFi.config(ip, gateway, subnet)){
+            Serial.println(F("\nErro na definição do IP."));
+            return false;
+        }
+    }
+
+    WiFi.mode(WIFI_STA);
+    WiFi.begin(root["ssid"].asString(), root["password"].asString());
+
+    for( int counter=0; WiFi.status()!=WL_CONNECTED && counter <30; counter++){
+        Serial.print(F("."));
+        delay(400);
+    }
+
+    if(WiFi.status() != WL_CONNECTED){
+        Serial.println("\nfalha ao logar em \""+WiFi.SSID()+"\"");
+        return false;
+    }
+
+    Serial.println("\nREDE: "+WiFi.SSID());
+    Serial.println("IP: "+WiFi.localIP().toString());
+    return true;
+}
+
+//###################################################################
+
+void modeAccessPoint(){
+
+}
